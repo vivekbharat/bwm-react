21a93405f10b19b49d7e8148cbc8799cbcbb8232
vivekbharat <vr329@njit.edu>
2018-10-19 06:56:36 -0700

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://vivek:password123@ds147942.mlab.com:47942/bwm-dev",
-  SECRET: "ndcucvy83478bvurv8dfg@&r34^#werweRTRF@#fewf^%^&%&^bjvhdsbv",
-  AWS_ACCESS_KEY_ID: "AKIAIX3KHDHWQKDT5JKA",
-  AWS_SECRET_ACCESS_KEY_ID: "1ZDkWgFdaOwsOzILmuSHW0DaHXwJ5AqYlDH/HXi8"
-};
